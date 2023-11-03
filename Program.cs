c0d80e1066549909bb3b8ce08a992169bbd257e8
Gábor Makó <makgab@gmail.com>
2023-11-03 12:35:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+
+//
+// Install package: System.Configuration.ConfigurationManager
+//
+
+namespace ConsoleAppConfigDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App config Demo app");
+
+            string sAttr;
+
+            // Read a particular key from the config file 
+            sAttr = ConfigurationManager.AppSettings.Get("Key0");
+            Console.WriteLine("The value of Key0: " + sAttr);
+
+            // Read all the keys from the config file
+            NameValueCollection sAll;
+            sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+                Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+System App Config Demo:
+
+ * https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/csharp/language-compilers/store-custom-information-config-file
+
+ * Install package: System.Configuration.ConfigurationManager
+
